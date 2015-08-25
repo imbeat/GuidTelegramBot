5c034a1dd7745d2b7339d073474065783df046c5
Navoznov Ivan <ivan.navoznov@gmail.com>
2015-08-25 12:29:35 +0300

    переименовал проект консольного бота

--- GuidTelegramBot.Console/Program.cs
+++ GuidTelegramBot.Console/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Threading.Tasks;
 using GuidTelegramBot.Core;
 
-namespace GuidTelegramBot
+namespace GuidTelegramBot.Console
 {
     public static class Program
     {
---
+++ GuidTelegramBot.WindowsService/GuidTelegramBotService.cs
@@ -0,0 +1,30 @@
+﻿using System.ServiceProcess;
+using System.Threading.Tasks;
+using GuidTelegramBot.Core;
+
+namespace GuidTelegramBot.WindowsService
+{
+    partial class GuidTelegramBotService : ServiceBase
+    {
+        public GuidTelegramBotService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            RunBot().Wait();
+        }
+
+        private async Task RunBot()
+        {
+            var guidBot = new GuidBot();
+            await guidBot.Do();
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: Add code here to perform any tear-down necessary to stop your service.
+        }
+    }
+}
