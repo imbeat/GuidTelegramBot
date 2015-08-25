726bc6cec093c4b454db7b31a0d21451ec926742
Navoznov Ivan <ivan.navoznov@gmail.com>
2015-08-25 12:26:23 +0300

    windows служба для бота + вынес код бота в отдельную class library + рефакторинг

--- GuidTelegramBot/Program.cs
+++ GuidTelegramBot/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Configuration;
-using System.Threading.Tasks;
-using Telegram.Bot;
+﻿using System.Threading.Tasks;
+using GuidTelegramBot.Core;
 
 namespace GuidTelegramBot
 {
@@ -13,29 +11,8 @@ namespace GuidTelegramBot
         }
         static async Task Run()
         {
-            var token = ConfigurationManager.AppSettings["apiToken"];
-            var bot = new Api(token);
-
-            var me = await bot.GetMe();
-
-            Console.WriteLine("Hello my name is {0}", me.Username);
-
-            var offset = 0;
-
-            while (true)
-            {
-                var updates = await bot.GetUpdates(offset);
-
-                foreach (var update in updates)
-                {
-                    if (update.Message.Text != null)
-                        await bot.SendTextMessage(update.Message.Chat.Id, Guid.NewGuid().ToString());
-
-                    offset = update.Id + 1;
-                }
-
-                await Task.Delay(1000);
-            }
+            var guidBot = new GuidBot();
+            await guidBot.Do();
         }
     }
 }
